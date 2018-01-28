115ab23e38e93c77fba32287a34c3e1e70778385
asbish <asobuishii@gmail.com>
2018-01-28 09:50:21 +0900

    Update babel and eslint rules

--- src/js/toggle.js
+++ src/js/toggle.js
@@ -6,13 +6,13 @@ export function toggleStyle(d, prop, vDef, v) {
   d.style[prop] = d.style[prop] === v ? vDef : v;
 }
 
-export function toggleTabIndexes(parent, query='a') {
+export function toggleTabIndexes(parent, query = 'a') {
   const xs = parent.querySelectorAll(query);
   for (let i = 0; i < xs.length; ++i) {
     const x = xs[i];
     x.setAttribute(
       'tabindex',
-      x.getAttribute('tabindex') === '-1' ? '0' : '-1'
+      x.getAttribute('tabindex') === '-1' ? '0' : '-1',
     );
   }
 }
