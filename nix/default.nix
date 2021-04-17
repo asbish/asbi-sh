7ca19c42b67910cbfecbef75fafc5a5fa5b22973
asbish <asobuishii@gmail.com>
2021-04-17 14:50:45 +0900

    upgrade node version

--- nix/default.nix
+++ nix/default.nix
@@ -3,20 +3,8 @@ import sources.nixpkgs {
   overlays = [
     (_: pkgs:
       let
-        nodejs = pkgs.nodejs-14_x.overrideAttrs (oldAttrs: rec {
-          version = "14.15.1";
-          name = "nodejs-${version}";
-          src = pkgs.fetchurl {
-            url = "https://nodejs.org/dist/v${version}/node-v${version}.tar.xz";
-            sha256 = "1g61vqsgq3jsipw2fckj68i4a4pi1iz1kbw7mlw8jmzp8rl46q81";
-          };
-        });
+        nodejs = pkgs.nodejs-14_x;
         yarn = pkgs.yarn.overrideAttrs (oldAttrs: rec {
-          version = "1.22.10";
-          src = pkgs.fetchurl {
-            url = "https://registry.npmjs.org/yarn/-/yarn-${version}.tgz";
-            sha512 = "IanQGI9RRPAN87VGTF7zs2uxkSyQSrSPsju0COgbsKQOOXr5LtcVPeyXWgwVa0ywG3d8dg6kSYKGBuYK021qeA==";
-          };
           buildInputs = [ nodejs ];
         });
         haskellPackages = pkgs.haskellPackages.override {
